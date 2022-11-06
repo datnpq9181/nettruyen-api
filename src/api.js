ad9ba0bfb9aa806b1200573832e3729336bfa247
Patronus <patronus@Patronus-Mac-mini.local>
2022-11-06 17:22:07 +0700

    update

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 
   res.header('Access-Control-Allow-Origin', 'https://my-manga-react.netlify.app');
 
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent');
 
   res.header('Host', 'https://my-manga-react.netlify.app');
   res.header('Referer', 'https://nettruyenin.com/');
