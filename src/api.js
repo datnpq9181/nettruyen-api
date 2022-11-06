34987621edf634fa872aa81cb0ecd82ef2e94d9a
Patronus <patronus@Patronus-Mac-mini.local>
2022-11-06 17:21:17 +0700

    update

--- src/api.js
+++ src/api.js
@@ -20,6 +20,7 @@ app.use(function (req, res, next) {
 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
+  res.header('Host', 'https://my-manga-react.netlify.app');
   res.header('Referer', 'https://nettruyenin.com/');
   next();
 });
